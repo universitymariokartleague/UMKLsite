85dc01c543b00643755b540664d67a8693cfb28d
zydezu <kinomaxxing@gmail.com>
2025-03-24 19:53:40 +0000

    yeah

---
+++ scripts/database.js
@@ -0,0 +1,45 @@
+const initSqlJs = window.initSqlJs;
+
+const config = {
+    locateFile: filename => `https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.5.0/${filename}`
+};
+
+const sqlPromise = initSqlJs(config);
+
+let db; // Define db in a broader scope
+
+async function initDatabase() {
+    const dataPromise = fetch("assets/database/umkl_db.db").then(res => res.arrayBuffer());
+    const [SQL, buf] = await Promise.all([sqlPromise, dataPromise]);
+    db = new SQL.Database(new Uint8Array(buf)); // Assign to the broader scoped db
+}
+
+async function executeSQL(sqlcmd) {
+    if (!db) {
+        console.error("Database is not initialized.");
+        return;
+    }
+
+    // Prepare a statement
+    const stmt = db.prepare(sqlcmd);
+    stmt.getAsObject({ $start: 1, $end: 1 }); // {col1:1, col2:111}
+
+    // Bind new values
+    stmt.bind({ $start: 1, $end: 2 });
+    while (stmt.step()) {
+        const row = stmt.getAsObject();
+        console.log('Here is a row: ' + JSON.stringify(row));
+    }
+}
+
+async function main() {
+    await initDatabase(); // Ensure the database is initialized
+    // executeSQL("SELECT * FROM tournament_entry");
+}
+
+main(); // Run the main function to initialize the database and execute SQL
+
+document.getElementById("executeSQLButton").addEventListener("click", async function() {
+    const sqlcmd = document.getElementById("SQLBox").value;
+    await executeSQL(sqlcmd);
+});
---
+++ scripts/settings.js
@@ -0,0 +1,118 @@
+// Setup
+const settingsBoxHTML = `
+<div class="hidden BGBlur" id="BGBlur"></div>
+<div class="hidden hide-settings-box" id="settingsBox">
+    <div class="settings-box-close-button">
+        <button id="settings-box-close-button">Close</button>
+    </div>
+    <div class="settings-title">Settings</div>
+    <div class="setting-options" id="settingsBoxJS">
+        <div class="setting-sub-heading">Apperance</div><hr />
+        Theme<button class="settings-option">Dark theme</button><br />
+        Ambient Mode <button class="settings-option">OFF</button>
+        <span class="settings-extra-info">(only available in dark theme)</span><br />
+    </div>
+</div>`;
+document.body.insertAdjacentHTML('beforeend', settingsBoxHTML);
+
+const BGBlur = document.getElementById('BGBlur');
+const settingsBox = document.getElementById('settingsBox');
+const settingsBoxJS = document.getElementById('settingsBoxJS');
+let settingsOpen = false;
+
+const toggleSettingsPanel = () => {
+    BGBlur.classList.toggle("hidden");
+    settingsBox.classList.toggle('hide-settings-box');
+    if (settingsOpen) {
+        
+    } else {
+        generateSettingsPanel();
+        settingsBox.classList.remove('hidden');
+        BGBlur.addEventListener("click", toggleSettingsPanel);
+    }
+    settingsOpen = !settingsOpen;
+};
+
+document.getElementById('settings-box-close-button').addEventListener('click', toggleSettingsPanel);
+document.querySelectorAll('.settingsBoxOpener').forEach(opener => {
+    opener.addEventListener('click', toggleSettingsPanel);
+});
+
+const meta = document.querySelector('meta[name="color-scheme"]');
+const root = document.querySelector(":root");
+let darkThemeEnabled;
+
+// Panel
+function generateSettingsPanel() {
+    let localStorageString = JSON.stringify(localStorage)
+    try {
+        settingsBoxJS.innerHTML = `
+        <div class="setting-sub-heading">Apperance</div><hr />
+        Theme<button id="toggleTheme" class="settings-option">${localStorage.getItem("darktheme") == 1 ? "Dark" : "Light"} theme</button><br />
+        Ambient Mode <button class="settings-option">OFF</button>
+        <span class="settings-extra-info">(only available in dark theme)</span><br />
+
+        <div class="setting-sub-heading">Localstorage<span class="settings-extra-info"> (data stored on your device)</span></div><hr />
+       
+        <code>${localStorageString = {} ? "No data stored" : localStorageString}</code><br/>
+        
+        Clear settings<button id="clearLocalStorage" class="settings-option">Clear</button>
+        <span class="settings-extra-info">(this will reload the page)</span></div><br />
+        `
+    } catch (error) {
+        settingsBoxJS.innerHTML = `<br/>Failed to load settings<br/><code>${error}</code>`
+    }
+    generateEventListeners();
+};
+generateSettingsPanel();
+
+
+function toggleTheme() {
+    darkThemeEnabled = darkThemeEnabled ? 0 : 1;
+    meta.content = darkThemeEnabled ? "dark" : "light";
+
+    if (darkThemeEnabled) {
+        console.log("Changing to dark theme...")
+        root.classList.add("dark-theme");
+        root.classList.remove("light-theme");
+    } else {
+        console.log("Changing to light theme...")
+        root.classList.add("light-theme");
+        root.classList.remove("dark-theme");  
+    }
+
+    localStorage.setItem("darktheme", darkThemeEnabled ? 1 : 0);
+
+    generateSettingsPanel();
+}
+
+function clearLocalStorage() {
+    localStorage.clear();
+    window.location.reload();
+}
+
+function generateEventListeners() {
+    document.getElementById('toggleTheme').addEventListener('click', toggleTheme);
+    document.getElementById('clearLocalStorage').addEventListener('click', clearLocalStorage);
+}
+
+function checkTheme() {
+    darkThemeEnabled = parseInt(localStorage.getItem("darktheme"));
+    if (darkThemeEnabled == true) {
+        meta.setAttribute('content', 'dark');
+        root.classList.add('dark-theme');
+        console.log("Set dark theme");
+    }
+    setTimeout(() => {
+        document.body.style.transition = "background-color 0.2s ease-out";
+    }, 0);
+}
+checkTheme();
+
+
+// keyboard shortcuts
+document.addEventListener('keydown', (event) => {
+    if ( event.key === 't') {
+        toggleTheme();
+    }
+});
