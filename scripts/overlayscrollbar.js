43eed16bb17370ff97620693eeb20cf6855a09d5
zydezu <kinomaxxing@gmail.com>
2025-04-02 05:55:21 +0100

    FIX: update OverlayScrollbars configuration to enable native scrollbars overlaid option

--- scripts/overlayscrollbar.js
+++ scripts/overlayscrollbar.js
@@ -4,18 +4,17 @@ OverlayScrollbars.plugin(ClickScrollPlugin);
 
 const osInstance = OverlayScrollbars({
     target: document.body,
+}, {
     cancel: {
         nativeScrollbarsOverlaid: true,
         body: null,
-    }
-}, {
+    },
     scrollbars: {
         theme: "os-theme-dark",
         clickScroll: true,
         autoHide: 'scroll',
         autoHideDelay: 3000,
-    },
-    nativeScrollbarsOverlaid: { initialize: false }
+    }
 });
 
 function changeScrollbarTheme(dark) {
