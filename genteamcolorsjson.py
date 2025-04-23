2850dc5ba98348c1fe35ca4aafe37729ca4e03c4
zydezu <kinomaxxing@gmail.com>
2025-04-23 20:56:46 +0100

    FEAT: add genteamcolorsjson.py

---
+++ genteamcolorsjson.py
@@ -0,0 +1,10 @@
+import logic
+import json
+
+def main():
+    teams_dict = {i[1]: i[3] for i in logic.get_teams_list()}
+    with open("database/teamcolors.json", "w") as file:
+        json.dump(teams_dict, file, indent=4)
+
+if __name__ == "__main__":
+    main()
