e95f9356aa5ba56482a8e15a660ac82ce1c75d3d
zydezu <kinomaxxing@gmail.com>
2025-05-05 18:26:40 +0100

    FIX: add loading indicator

--- scripts/copystandingsimage.js
+++ scripts/copystandingsimage.js
@@ -179,10 +179,10 @@ shareButton.addEventListener("click", async () => {
     if (isPopupShowing) return;
     
     try {
+        shareButton.innerHTML = "Loading shareable image...";
         const blob = await fetch(`assets/pythongraphics/output/team_standings_season${currentSeason.value}.png`).then(r => r.blob());
 
         if (isWindowsOrLinux() || !navigator.canShare) {
-            shareButton.innerHTML = "Loading clipboard image...";
             const success = await copyImageToClipboard(blob);
             shareButton.innerText = success ? "Image copied to clipboard!" : "Failed to copy!";
 
