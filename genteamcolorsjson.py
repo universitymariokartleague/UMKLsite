d9948ec00a33d2595e428dadc018fb56aaf8e6da
zydezu <kinomaxxing@gmail.com>
2025-06-01 14:57:02 +0100

    FIX: update internal scripts

--- genteamcolorsjson.py
+++ genteamcolorsjson.py
@@ -1,12 +1,13 @@
-# Please use .runbeforecommit.py
-
 import logic as logic
 import json
 
 def generate_team_colors_json():
-    teams_dict = {i[1]: i[3] for i in logic.get_teams_list()}
+    teams_list = [
+        {"team_color": i[3], "team_name": i[1]}
+        for i in logic.get_teams_list()
+    ]
     with open("database/teamcolorsfallback.json", "w") as file:
-        json.dump(teams_dict, file, indent=4)
+        json.dump(teams_list, file, indent=4)
 
 # if __name__ == "__main__":
 #     generate_team_colors_json()
