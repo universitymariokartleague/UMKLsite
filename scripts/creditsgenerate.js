ecc466d210014e2c0c48359275d90cda44bcc459
zydezu <kinomaxxing@gmail.com>
2025-06-28 02:02:44 +0100

    FIX: fix links

--- scripts/creditsgenerate.js
+++ scripts/creditsgenerate.js
@@ -6,34 +6,34 @@
 const contributors = [
     {
         name: "h.omeless",
-        img: "https://cdn.discordapp.com/avatars/330762336669532161/05a6769f5dc50de87d584c49bcc9adf6?size=1024",
+        img: "https://cdn.discordapp.com/avatars/330762336669532161/05a6769f5dc50de87d584c49bcc9adf6.png?size=1024",
         discord: "https://discordapp.com/users/330762336669532161",
         github: "https://github.com/h-omeless/"
     },
     {
         name: "redthe1",
-        img: "https://cdn.discordapp.com/avatars/326369832129921024/3feb1250000b858f69afb25a2166d143?size=1024",
+        img: "https://cdn.discordapp.com/avatars/326369832129921024/3feb1250000b858f69afb25a2166d143.png?size=1024",
         discord: "https://discordapp.com/users/326369832129921024"
     },
     {
         name: "skjamm",
-        img: "https://cdn.discordapp.com/avatars/309400933870206978/fa52349be7108e85402f5477e548bc77?size=1024",
+        img: "https://cdn.discordapp.com/avatars/309400933870206978/fa52349be7108e85402f5477e548bc77.png?size=1024",
         discord: "https://discordapp.com/users/309400933870206978"
     },
     {
         name: "theenderdiamond",
-        img: "https://cdn.discordapp.com/avatars/427910181830131712/25384229bd2816748fec7742ec9a7371?size=1024",
+        img: "https://cdn.discordapp.com/avatars/427910181830131712/25384229bd2816748fec7742ec9a7371.png?size=1024",
         discord: "https://discordapp.com/users/427910181830131712"
     },
     {
         name: "tsun1509",
-        img: "https://cdn.discordapp.com/avatars/342974732938903563/ecd8f2918246d6d90ab60629764e9151?size=1024",
+        img: "https://cdn.discordapp.com/avatars/342974732938903563/ecd8f2918246d6d90ab60629764e9151.png?size=1024",
         discord: "https://discordapp.com/users/342974732938903563",
         github: "https://github.com/TusharSundarka/"
     },
     {
         name: "zyposts",
-        img: "https://cdn.discordapp.com/avatars/1202021758685949994/64d75ad82deb314036e6b5113ae95a58?size=1024",
+        img: "https://cdn.discordapp.com/avatars/1202021758685949994/64d75ad82deb314036e6b5113ae95a58.png?size=1024",
         discord: "https://discordapp.com/users/1202021758685949994",
         github: "https://github.com/zydezu/",
         email: "mailto:webmaster@umkl.co.uk"
