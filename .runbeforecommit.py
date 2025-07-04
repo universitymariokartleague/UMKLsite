9972cc0bfd8fcc1e67783aacbe0cdbee52e87bc5
zydezu <kinomaxxing@gmail.com>
2025-07-04 23:10:15 +0100

    FIX: update automated fallback scripts

--- .runbeforecommit.py
+++ .runbeforecommit.py
@@ -1,6 +1,6 @@
 # This script is intended to be run before committing changes to ensure that the latest graphics and JSON files are generated.
 
-import genteamcolorsjson, graphics, logic, makesitemap
+import genteamdata, graphics, logic, makesitemap
 
 def run_pre_commit_tasks():
     """
@@ -9,15 +9,8 @@ def run_pre_commit_tasks():
     This function generates graphics for team standings for all seasons,
     and then generates a JSON file containing team colors.
     """
-    # Generate the graphics
-    # print("Generating graphics...")
-    # max_season = logic.get_current_season()
-    # for i in range(1, max_season + 1):
-    #     graphics.create_team_standings_image(season_id=i, add_timestamp=True)
-
-    # Generate the team colors JSON file
-    print("Generating team colors JSON file...")
-    genteamcolorsjson.generate_team_colors_json()
+    print("Generating teamdata/matchdata fallback JSON files...")
+    genteamdata.generate_match_data_json()
 
     # Generate sitemap
     print("Generating sitemap...")
---
+++ genteamdata.py
@@ -0,0 +1,39 @@
+import json, requests
+
+API_URL = "https://api.umkl.co.uk/"
+HEADERS = {"Content-Type": "application/json"}
+
+def get_current_season():
+    response = requests.post(f"{API_URL}seasoninfo", json={"season": ""}, headers=HEADERS)
+    if response.content:
+        return response.json()[0]
+
+def get_teamdata(payload):
+    response = requests.post(f"{API_URL}teamdata", json=payload, headers=HEADERS)
+    if response.content:
+        return response.json()
+
+def get_teamcolors():
+    response = requests.post(f"{API_URL}teamcolors", json={}, headers=HEADERS)
+    if response.content:
+        return response.json()
+
+def get_matchdata():
+    response = requests.post(f"{API_URL}matchdata", json={}, headers=HEADERS)
+    if response.content:
+        return response.json()
+
+def generate_match_data_json():
+    current_season = get_current_season()
+    for i in range(1, current_season + 1):
+        with open(f"database/teamdatafallbacks{i}.json", "w") as file:
+            json.dump(get_teamdata({"team": "", "season": i}), file, indent=4)
+
+    with open("database/teamcolorsfallback.json", "w") as file:
+        json.dump(get_teamcolors(), file, indent=4)
+
+    with open("database/matchdatafallback.json", "w") as file:
+        json.dump(get_matchdata(), file, indent=4)
+
+if __name__ == "__main__":
+    generate_match_data_json()
