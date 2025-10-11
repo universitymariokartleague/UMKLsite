c5a06859ca1b41557dc0c6166e2bf4e93f40c69c
zydezu <kinomaxxing@gmail.com>
2025-10-11 21:44:16 +0100

    FIX: edit update rates in getliveresults.js

--- scripts/getliveresults.js
+++ scripts/getliveresults.js
@@ -214,7 +214,7 @@ document.addEventListener("DOMContentLoaded", async () => {
             }
             console.debug(`%cgetlivedata.js %c> %cRefreshing live data...`, "color:#fc52ff", "color:#fff", "color:#fda6ff");
 
-            if (!window.lastMatchUpdate || Date.now() - window.lastMatchUpdate >= 30000) {
+            if (!window.lastMatchUpdate || Date.now() - window.lastMatchUpdate >= 120000) {
                 matchData = await getMatchData();
                 getLiveMatchTeams();
                 window.lastMatchUpdate = Date.now();
@@ -226,9 +226,9 @@ document.addEventListener("DOMContentLoaded", async () => {
         } catch (error) {
             errorMessage.innerHTML = `Retrying: attempt ${window.retryCount}`;
         } finally {
-            refreshTimer = setTimeout(updateFetch, 2000);
+            refreshTimer = setTimeout(updateFetch, 3000);
         }
     };
 
-    refreshTimer = setTimeout(updateFetch, 2000);
+    refreshTimer = setTimeout(updateFetch, 3000);
 });
