58dc34630e3cc9df82ec2326aa94ac552e5aa276
zydezu <kinomaxxing@gmail.com>
2026-02-04 01:49:43 +0000

    FIX: allow clicking the profile pictures on the credits page

--- scripts/creditsgenerate.js
+++ scripts/creditsgenerate.js
@@ -75,7 +75,7 @@ function renderContributor(person) {
 
     return `
         <div translate="no" class="credit-container">
-            <img onload="this.style.opacity=1" loading="lazy" class="credit-image" src="assets/media/credits/${person.name}.avif">
+            <a href="${person.discord}"><img onload="this.style.opacity=1" loading="lazy" class="credit-image" src="assets/media/credits/${person.name}.avif"></a>
             <div class="credit-info">
                 <b>${person.name}</b>
                 <div class="credit-socials">${socials}</div>
